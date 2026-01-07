150efc7b316c7da9865a8d42f61de1987888abc7
24011254 <24011254@myrp.edu.sg>
2026-01-07 15:57:15 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 3000;
 //database config info
 const dbConfig = {
     host: process.env.DB_HOST,
-    user: process.env.SB_USER,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
